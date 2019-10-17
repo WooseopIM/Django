160a9fb0182d48b8c973b8b757b1785883221304
WooseopIM <wooseopim93@gmail.com>
2019-10-17 18:45:59 +0900

    CRUD_MEDIA

---
+++ CRUD_MEDIA/posts/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    image = models.ImageField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-pk']
+
+class Comment(models.Model):
+    content = models.CharField(max_length=300)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    # models.OneToOne()
+    # models.ManyToMany()
+
+    class Meta:
+        ordering = ['-pk']
